01840f459f8b8f481ccb48250cbba1637e4578fe
Santiago Ceron <santiceron023@gmail.com>
2019-12-03 14:08:12 -0500

    test quemado

---
+++ AdminTenisPark/src/main/java/com/ceiba/AdminTenisPark/infraestructura/controller/PrecioController.java
@@ -0,0 +1,22 @@
+package com.ceiba.AdminTenisPark.infraestructura.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/tarifa")
+public class PrecioController {
+	
+//	private final ManejadorCrearPrecio  manejadorCrearPrecio;
+//	
+//	public ControladorCliente(ManejadorCrearCliente manejadorCrearCliente) {
+//		this.manejadorCrearCliente = manejadorCrearCliente;
+//	}
+//	
+//	@PostMapping("/crear")
+//	public void crearCliente(@RequestBody ComandoCliente comandoCliente) {
+//		this.manejadorCrearCliente.ejecutar(comandoCliente);
+//	}
+	
+}
--- AdminTenisPark/src/test/java/com/ceiba/AdminTenisPark/AdminTenisParkApplicationTests.java
+++ AdminTenisPark/src/test/java/com/ceiba/AdminTenisPark/AdminTenisParkApplicationTests.java
@@ -1,13 +1,27 @@
 package com.ceiba.AdminTenisPark;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class AdminTenisParkApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	public void generarGarantiaTest() {
 
+		// arrange
+		Boolean a= true;
+//		Producto producto = new ProductoTestDataBuilder().conNombre(COMPUTADOR_LENOVO).build();
+//		repositorioProducto.agregar(producto);
+//		Vendedor vendedor = new Vendedor(repositorioProducto, repositorioGarantia);
+
+		// act
+//		vendedor.generarGarantia(producto.getCodigo(), NOMBRE_CLIENTE);
+
+		// assert
+		Assert.assertTrue(a);
+
+	}
 }
