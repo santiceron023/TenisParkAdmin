d1d2c79b4815eda1b78a4ce6e128351d990db812
Santiago Ceron <santiago.ceron@ceiba.com.co>
2019-12-04 14:13:16 -0500

    se corrige agrega swagger

--- AdminTenisPark/src/main/java/com/ceiba/AdminTenisPark/infraestructura/repositorio/adapter/RepositorioReservaPosgress.java
+++ AdminTenisPark/src/main/java/com/ceiba/AdminTenisPark/infraestructura/repositorio/adapter/RepositorioReservaPosgress.java
@@ -1,22 +1,23 @@
 package com.ceiba.AdminTenisPark.infraestructura.repositorio.adapter;
 
-
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Repository;
-
 import com.ceiba.AdminTenisPark.dominio.modelo.Reserva;
 import com.ceiba.AdminTenisPark.dominio.puerto.repositorio.RepositorioReserva;
+import com.ceiba.AdminTenisPark.infraestructura.repositorio.entity.ReservaEntity;
 import com.ceiba.AdminTenisPark.infraestructura.repositorioJpa.RepositorioReservaJpa;
 
+
 @Repository
-public class RepositorioReservaImpl implements RepositorioReserva {
+public class RepositorioReservaPosgress implements RepositorioReserva {
 	
 	private ModelMapper modelMapper = new ModelMapper();
 	private RepositorioReservaJpa repositorioReservaJpa;
 
 	@Override
 	public void crear(Reserva reserva) {
-				
+		  ReservaEntity reservaEntity = modelMapper.map(reserva, ReservaEntity.class);
+		  repositorioReservaJpa.save(reservaEntity);				
 	}
 
 }
