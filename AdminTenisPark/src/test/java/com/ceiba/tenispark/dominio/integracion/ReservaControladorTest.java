412c412590bdce60ece06b0156f3b9bc9c8d254c
Santiago Ceron <santiago.ceron@ceiba.com.co>
2019-12-06 19:00:47 -0500

    jenkins test debug

--- AdminTenisPark/src/test/java/com/ceiba/tenispark/dominio/integracion/ReservaControladorTest.java
+++ AdminTenisPark/src/test/java/com/ceiba/tenispark/dominio/integracion/ReservaControladorTest.java
@@ -1,62 +1,62 @@
-//package com.ceiba.tenispark.dominio.integracion;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import org.junit.Before;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.TestPropertySource;
-//import org.springframework.test.context.jdbc.Sql;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.web.context.WebApplicationContext;
-//
-//import com.ceiba.tenispark.AdminTenisParkApplication;
-//import com.ceiba.tenispark.aplicacion.comando.ReservaComando;
-//import com.ceiba.tenispark.dominio.testDataBuilder.ReservaComandoTestDataBuilder;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = AdminTenisParkApplication.class)
-//@AutoConfigureMockMvc
-//@TestPropertySource("/test.properties")
-//public class ReservaControladorTest {
-//
-//	@Autowired
-//	private WebApplicationContext wac;
-//	@Autowired
-//	private ObjectMapper objectMapper;
-//
-//	@Autowired
-//	private MockMvc mockMvc;
-//
-//	@Before
-//	public void setup() throws Exception {
-//		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();				
-//	}
-//
-//	
-//	@Test
-//	@Sql(scripts = "/preCondicionesReserva.sql",
-//	executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-//	public void crearReserva() throws Exception {
-//		//arrange
-//		ReservaComando comandoFactura = new ReservaComandoTestDataBuilder().build();
-//		//SQL FILE
-//		
-//		//Act-Assert
-//		mockMvc.perform(post("/reserva")
-//				.contentType(MediaType.APPLICATION_JSON)
-//				.content(objectMapper.writeValueAsString(comandoFactura))
-//				.accept(MediaType.APPLICATION_JSON))
-//		.andDo(print())
-//		.andExpect(status().isCreated());
-//	}
-//	
-//}
+package com.ceiba.tenispark.dominio.integracion;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.ceiba.tenispark.AdminTenisParkApplication;
+import com.ceiba.tenispark.aplicacion.comando.ReservaComando;
+import com.ceiba.tenispark.dominio.testDataBuilder.ReservaComandoTestDataBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AdminTenisParkApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource("/test.properties")
+public class ReservaControladorTest {
+
+	@Autowired
+	private WebApplicationContext wac;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();				
+	}
+
+	
+	@Test
+	@Sql(scripts = "/preCondicionesReserva.sql",
+	executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	public void crearReserva() throws Exception {
+		//arrange
+		ReservaComando comandoFactura = new ReservaComandoTestDataBuilder().build();
+		//SQL FILE
+		
+		//Act-Assert
+		mockMvc.perform(post("/reserva")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(comandoFactura))
+				.accept(MediaType.APPLICATION_JSON))
+		.andDo(print())
+		.andExpect(status().isCreated());
+	}
+	
+}
