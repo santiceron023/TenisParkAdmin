07c00f354c93901d99f07608c9738d1e374b3786
Santiago Ceron <santiago.ceron@ceiba.com.co>
2019-12-13 12:39:35 -0500

    se agregan valdiaciones de cancha

--- AdminTenisPark/src/main/java/com/ceiba/tenispark/dominio/modelo/Cancha.java
+++ AdminTenisPark/src/main/java/com/ceiba/tenispark/dominio/modelo/Cancha.java
@@ -1,11 +1,16 @@
 package com.ceiba.tenispark.dominio.modelo;
 
+import com.ceiba.tenispark.dominio.util.ValidadorArgumentos;
+
 public class Cancha {
 
 	private Integer id;
 	private String administrador;
 	private String telefono;
 	public Cancha(Integer id, String administrador, String telefono) {
+		ValidadorArgumentos.validarNoVacio(telefono);
+		ValidadorArgumentos.validarNoVacio(id);
+		ValidadorArgumentos.validarNoVacio(administrador);
 		this.id = id;
 		this.telefono = telefono;
 		this.administrador = administrador;
